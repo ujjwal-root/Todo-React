464d3548870e224bbcd442b9d92cbb1c03f5a65e
Your ujjwal-root <anandujjwalbns@gmail.com>
2024-07-06 22:58:14 +0530

    New React Practice Project

---
+++ src/MyComponents/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+        <b>About To-Do List Manager</b>
+      <p> Welcome to the To-Do List Manager, your ultimate solution for organizing tasks and boosting productivity. This app, meticulously crafted to provide a seamless and efficient way to manage your daily to-do lists.<br></br>
+          
+          
+        <br></br><b>Features:</b><br></br>
+          Add and Delete Tasks: Easily add new tasks and delete completed or unnecessary ones to keep your list up-to-date.<br></br>
+          Whether you're a student, professional, or someone who simply loves staying organized, our To-Do List Manager is designed to cater to your needs.<br></br>
+        
+        
+        
+        
+<br></br><b>Thank you for using the To-Do List Manager. Your feedback and suggestions are always welcome!</b><br></br>
+      </p>
+    </div>
+  );
+};
